0891e5200731e468242b84cac985ebf0d69c0c26
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-09-15 15:34:10 -0300

    feat: enhance user creation logic and clean up user list component

--- src/app/hooks/useCreaterUser.ts
+++ src/app/hooks/useCreaterUser.ts
@@ -1,15 +1,20 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createUser } from "../services/createUsers";
+import type { IUser } from "../types/IUser";
 import { USERS_QUERY_KEY } from "./useUsers";
 
 export function useCreateUser() {
   const queryClient = useQueryClient();
   const { mutateAsync, isPending } = useMutation({
-    mutationKey: ["createUser"],
     mutationFn: createUser,
-    onSuccess: () => {
-      // Aqui você pode invalidar queries ou atualizar o cache, se necessário
-      return queryClient.invalidateQueries({ queryKey: USERS_QUERY_KEY });
+    //onMutate - executa diretamente antes de executar a função
+    onMutate: (variables) => {
+      queryClient.setQueryData<IUser[]>(USERS_QUERY_KEY, (old) =>
+        old?.concat({
+          ...variables,
+          id: String(Math.random()),
+        })
+      );
     },
   });
 
--- src/components/UsersLists.tsx
+++ src/components/UsersLists.tsx
@@ -1,7 +1,5 @@
 import { useUpdateUser } from "@/app/hooks/useUpdateUser";
 import { useUsers } from "@/app/hooks/useUsers";
-import type { IUser } from "@/app/types/IUser";
-import { useMutationState } from "@tanstack/react-query";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Skeleton } from "./ui/skeleton";
 import { Switch } from "./ui/switch";
@@ -9,14 +7,6 @@ import { Switch } from "./ui/switch";
 export function UsersLists() {
   const { users, isLoading } = useUsers();
   const { updateUser } = useUpdateUser();
-
-  const pendingUsers = useMutationState({
-    filters: {
-      status: "pending",
-      mutationKey: ["createUser"],
-    },
-    select: (mutation) => mutation.state.variables as Omit<IUser, "id">,
-  });
 
   async function handleBlockedChange(id: string, blocked: boolean) {
     await updateUser({
@@ -52,29 +42,6 @@ export function UsersLists() {
             className="mt-4"
             //checked={user.blocked}
             onCheckedChange={(blocked) => handleBlockedChange(user.id, blocked)}
-          />
-        </div>
-      ))}
-
-      {pendingUsers.map((user) => (
-        <div
-          key={String(Math.random())}
-          className="p-4 mb-4 border rounded-lg flex flex-row justify-between "
-        >
-          <div>
-            <Avatar>
-              <AvatarImage src={`https://github.com/${user.username}.png`} />
-              <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
-            </Avatar>
-            <h2 className="text-xl font-bold">
-              {user.name} ({user.username})
-            </h2>
-          </div>
-
-          <Switch
-            className="mt-4"
-            //checked={user.blocked}
-            //onCheckedChange={(blocked) => handleBlockedChange(user.id, blocked)}
           />
         </div>
       ))}
